42f15e383f990be35d206cce42d9358ca6890f21
Foras00 <maikerusenpai01@gmail.com>
2022-03-29 00:25:55 +0800

    admin page changes & animation triggers
    added hamburger navigation button on admin page and used jQuery for UI script

--- script/admin-page.js
+++ script/admin-page.js
@@ -1,41 +1,39 @@
-function extendBar(){
-    document.getElementById('nav').style.height = "20em";
-}
-function closeBar(){
-    document.getElementById('nav').style.height = "4em";
-}
 
-function extendBarRegister(){
-    document.getElementById('nav').style.height = "20em";
-    document.getElementById('register-tab').style.height = "20em";
-}
-function closeBarRegister(){
-    document.getElementById('nav').style.height = "4em";
-    document.getElementById('register-tab').style.height = "4em";
-}
-function extendBarAccount(){
-    document.getElementById('nav').style.height = "20em";
-    document.getElementById('account-tab').style.height = "20em";
-}
-function closeBarAccount(){
-    document.getElementById('nav').style.height = "4em";
-    document.getElementById('account-tab').style.height = "4em";
-}
+$(document).ready(function(){
+    $("#hamburger-button").click(function(){
+        $('.side-nav').toggleClass('sn-slide');
+    
+    });
+    $("#register-tab").click(function(){
+        $('.register-tab').toggleClass('sn-content-slide');
+    
+    });
+    $("#account-tab").click(function(){
+        $('.account-tab').toggleClass('sn-content-slide');
+    
+    });
+          
+});
 
-function goBack(){
+
+
+
+function goBack() {
     document.write("<?php session_destroy();?>")
     window.location = "../index.php";
 }
 var logcon = document.getElementById('logout-confirmation');
-function  logOutPressed(){
-    
+function logOutPressed() {
+
     logcon.classList.toggle("m-fadeIn");
 
     document.getElementById('logout-confirmation').style.width = "15em";
     document.getElementById('logout-confirmation').style.height = "10em";
+    document.getElementById('logout-confirmation').style.visibility = "visible";
 }
-function logOutNoPressed(){
+function logOutNoPressed() {
     logcon.classList.toggle("m-fadeOut");
     document.getElementById('logout-confirmation').style.height = "0";
     document.getElementById('logout-confirmation').style.width = "0";
 }
+
--- script/index.js
+++ script/index.js
@@ -1,25 +1,28 @@
-function chkBarcodeInput(){
-    if(document.getElementById('barcode-input').value === ""){
-        alert("barcode form is empty");
-    }
-}
-function chkAdminForm(){
-    var id = document.getElementById('admin-id');
-    var pass = document.getElementById('admin-pass');
-    var err = document.getElementById('admin-form-error');
-    if(id.value == "" && pass.value == ""){
-        alert("Both fields is empty!");
-    }else if(id.value == "" && pass.value != ""){
-        alert("Field: Admin ID is empty!");
-    }else if(id.value != "" && pass.value == ""){
-        alert("Field: Password is empty!");
-    }
-}
+$(document).ready(function(){
 
+    $('#admin-login').click(function(){
+        $('.admin-login-screen').toggleClass('lf-admin');
+    });
 
-function openAdminForm(){
-    document.getElementById('admin-login-page').style.width = "21em";
-}
-function closeAdminForm(){
-    document.getElementById('admin-login-page').style.width = "0";
-}
+    $('#closebtn').click(function(){
+        $('.admin-login-screen').toggleClass('lf-admin');
+    });
+
+    $('#admin-form').submit(function(){
+        var id = document.getElementById('admin-id');
+        var pass = document.getElementById('admin-pass');
+        var err = document.getElementById('admin-form-error');
+        if(id.value == "" && pass.value == ""){
+            alert("Both fields is empty!");
+        }else if(id.value == "" && pass.value != ""){
+            alert("Field: Admin ID is empty!");
+        }else if(id.value != "" && pass.value == ""){
+            alert("Field: Password is empty!");
+        }
+    });
+    $('#barcode-form').submit(function(){
+        if(document.getElementById('barcode-input').value === ""){
+            alert("barcode form is empty");
+        }
+    });
+});
