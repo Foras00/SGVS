0cb1909884c1ad9cc73c1ea9900039a3507cb4b5
Foras00 <maikerusenpai01@gmail.com>
2022-03-30 05:28:45 +0800

    (Admin page) fixed nav and prepared main container
    made top and side navigation bar fixed, added content-container in preparation for the addition of other sections e.g. register voter, register candidate, change password, etc.
    
    also removed the blurred background and replaced it with plain color :D

--- script/admin-page.js
+++ script/admin-page.js
@@ -12,29 +12,17 @@ $(document).ready(function(){
         $('.account-tab').toggleClass('sn-content-slide');
     
     });
-          
+    $("#logout-button").click(function(){
+        $('.logout-container').toggleClass('lo-slide');
+    });
+    $('#y-button').click(function(){
+        document.write("<?php session_destroy();?>")
+        window.location = "../index.php";
+    });
+    $("#n-button").click(function(){
+        $('.logout-container').toggleClass('lo-slide');
+    });
+
 });
 
-
-
-
-function goBack() {
-    document.write("<?php session_destroy();?>")
-    window.location = "../index.php";
-}
-var logcon = document.getElementById('logout-confirmation');
-function logOutPressed() {
-
-    logcon.classList.toggle("m-fadeIn");
-
-    document.getElementById('logout-confirmation').style.width = "15em";
-    document.getElementById('logout-confirmation').style.height = "10em";
-    document.getElementById('logout-confirmation').style.visibility = "visible";
-    document.getElementById('logout-confirmation').style.zIndex = "1";
-}
-function logOutNoPressed() {
-    logcon.classList.toggle("m-fadeOut");
-    document.getElementById('logout-confirmation').style.height = "0";
-    document.getElementById('logout-confirmation').style.width = "0";
-}
 
